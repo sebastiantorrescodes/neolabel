1a4bd7758ee9c0d3ef45d2ff8d60ae54499c2a93
Sebastiantorrescodes <sebastian.torres.codes@gmail.com>
2025-08-02 18:30:18 -0500

    Basic Landing Page Design

--- neolabel/app/page.tsx
+++ neolabel/app/page.tsx
@@ -1,3 +1,48 @@
+import "@/app/ui/global.css";
+import Image from "next/image";
+import Link from "next/link";
+import NavBar from "@/app/ui/navbar";
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <>
+      <NavBar />
+      <div className="flex h-screen items-center justify-center px-6">
+        <div className="flex max-w-7xl w-full flex-col md:flex-row items-center justify-between gap-10">
+          <div className="w-full md:w-1/2">
+            <h1 className="py-6 text-5xl font-semibold text-black">
+              WELCOME TO <span className="text-primary">NEOLABEL</span>
+            </h1>
+            <p className="text-2xl font-medium text-black">
+              Intelligently Rename Your Photos Using AI By Extracting Key
+              Metadata and Subject For Clean, Organized Filenames.
+            </p>
+            <div className="flex gap-5">
+              <Link
+                href="/features"
+                className="w-50 flex items-center justify-center rounded-3xl bg-primary p-3 mt-6 text-sm font-medium text-white shadow-xl"
+              >
+                <span>Explore Features</span>
+              </Link>
+              <Link
+                href="/login"
+                className="w-40 flex items-center justify-center rounded-3xl bg-primary p-3 mt-6 text-sm font-medium text-white shadow-xl"
+              >
+                <span>Get Started</span>
+              </Link>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2">
+            {/* <Image
+              src="/neoLabelHero.svg"
+              width={600}
+              height={600}
+              className="w-full max-w-md"
+              alt="Screenshots of the dashboard project showing desktop version"
+            /> */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ neolabel/app/ui/navbar.tsx
@@ -0,0 +1,19 @@
+export default function NavBar() {
+  return (
+    <nav className=" sticky top-0 z-50 flex justify-between  items-center  h-6 m-8 p-8 rounded-lg bg-linear-to-r from-accent from-0% to-secondary to-50%">
+      <div>
+        <h1 className="text-2xl font-medium text-white ">NEOLABEL</h1>
+      </div>
+      <div className=" flex text-base font-medium text-white gap-5 ">
+        <h1>Features</h1>
+        <h1>Pricing</h1>
+        <h1>About Us</h1>
+        <h1>Contact</h1>
+      </div>
+      <div className="flex text-base font-medium text-white gap-5 ">
+        <h1>Sign Up</h1>
+        <h1>Login</h1>
+      </div>
+    </nav>
+  );
+}
